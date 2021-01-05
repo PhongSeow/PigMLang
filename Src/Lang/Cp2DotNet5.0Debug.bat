eedd169ac08be779e34efd9ea9f13c89515ac895
PhongSeow <seowphong@hotmail.com>
2021-01-05 08:00:54 +0000

    Delete Src/Lang

--- Src/Lang/Cp2DotNet5.0Debug.bat
+++
@@ -1,3 +0,0 @@
-cd /d %cd%
-xcopy .\PigMLang.* ..\DotNet5.0\PigMLang\PigMLang\bin\Debug\net5.0\Lang /y /d
-pause
--- Src/Lang/Cp2DotNet5.0Release.bat
+++
@@ -1,3 +0,0 @@
-cd /d %cd%
-xcopy .\PigMLang.* ..\DotNet5.0\PigMLang\PigMLang\bin\Release\net5.0\Lang /y /d
-pause
